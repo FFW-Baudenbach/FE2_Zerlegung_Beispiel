242fb666df847087cedd0f56abb703746b5e7077
Markus Zellner <markus@zellner-mail.de>
2020-11-14 20:16:18 +0100

    Initial commit

---
+++ src/main/java/fe2/zerlegung/ILSAnsbach.java
@@ -0,0 +1,68 @@
+package fe2.zerlegung;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import de.alamos.fe2.external.enums.EAlarmDataEntries;
+import de.alamos.fe2.external.interfaces.IAlarmExtractor;
+
+/**
+ * Zerlegung für Alarmfax der ILS Ansbach.
+ */
+public class ILSAnsbach implements IAlarmExtractor {
+
+	/**
+	 * Zerlegt den Textinput von der Texterkennung und extrahiert
+	 * Werte in Key/Value Paare für die Weiterverarbeitung mit FE2.
+	 * 
+	 * @param input Input String von Texterkennung.
+	 * @return Map von Parametern mit Werten.
+	 */
+	@Override
+	public Map<String, String> extract(String input) {
+		
+		/*
+		 * In dieser Map können alle Parameter gesetzt werden,
+		 * die über eine normale Zerlegung auch gesetzt werden können
+		 */
+		
+		Map<String, String> data = new HashMap<>();
+		
+		/*
+		 * Beispielzerlegungen
+		 */
+		
+		//BEISPIEL 1
+
+		//Teile übergebenen String an Trennzeichen "&"
+		String[] parameters = input.split("&");
+		
+		//Stichwort steht an erster Stelle, daher [0]
+		String keyword = parameters[0];
+		data.put("keyword", keyword);
+		
+		//Straße steht an dritter Stelle, daher [2]
+		String street = parameters[2];
+		data.put("street", street);
+
+		
+		//BEISPIEL 2
+		/*
+		 * Hier werden die Parameter in der Map mit vordefinierten Enums aus dem Enum-Objekt "EAlarmDataEntries" gesetzt. 
+		 * Hier sind nicht alle Parameter hinterlegt. 
+		 * Sollte einer fehlen, verwenden Sie bitte den normalen Parametername, z. B. "mitteiler"
+		 */
+		
+		//Stichwort ist immer 6 Stellen lang und immer am Anfang der Nachricht
+		String stichwort = input.substring(0, 6);
+		data.put(EAlarmDataEntries.KEYWORD.getKey(), stichwort);
+		
+		//Straße wird extrahiert und anschließend werden alle Kommas
+		String strasse = input.substring(6, 30).replace(",", "");
+		data.put(EAlarmDataEntries.STREET.getKey(), strasse);
+		
+
+		//Diese Zeile muss ganz am Ende stehen, nachdem die eigene Zerlegung durchgefßhrt wurde. Hier wird die Map an FE2 zurßck gegeben
+		return data;
+	}
+}
---
+++ src/test/java/fe2/zerlegung/test/ILSAnsbachTest.java
@@ -0,0 +1,39 @@
+package fe2.zerlegung.test;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Map;
+
+import fe2.zerlegung.ILSAnsbach;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import de.alamos.fe2.external.enums.EAlarmDataEntries;
+
+public class ILSAnsbachTest {
+
+	@Test
+	@Ignore("Failing at the moment")
+	public void test() {
+		ILSAnsbach impl = new ILSAnsbach();
+
+		//BEISPIELDATEN
+		
+		String msg = "B 3 PERSON&Brand Zimmer&Teststrasse 12&Augsburg&EPN123&Anrufer meldet schwarzen Rauch aus Fenster";
+		Map<String, String> map = impl.extract(msg);
+		assertEquals("B 3 PERSON", map.get("keyword"));
+		assertEquals("Brand Zimmer", map.get("keyword_description"));
+		assertEquals("Teststraße 12", map.get(EAlarmDataEntries.STREET.getKey()));
+		assertEquals("Augsburg", map.get(EAlarmDataEntries.CITY.getKey()));
+		assertEquals("EPN123", map.get("epn"));
+		assertEquals("Anrufer meldet schwarzen Rauch aus Fenster", map.get("text"));
+
+		msg = "B BMA&Brandmeldeanlage (Feuermeldung)&Musterstrasse 12-14&Augsburg&EPN456";
+		map = impl.extract(msg);
+		assertEquals("B BMA", map.get("keyword"));
+		assertEquals("Brandmeldeanlage (Feuermeldung)", map.get(EAlarmDataEntries.KEYWORD_DESCRIPTION.getKey()));
+		assertEquals("Musterstrasse 12-14", map.get("street"));
+		assertEquals("Augsburg", map.get(EAlarmDataEntries.CITY.getKey()));
+		assertEquals("EPN456", map.get("epn"));
+	}
+}
