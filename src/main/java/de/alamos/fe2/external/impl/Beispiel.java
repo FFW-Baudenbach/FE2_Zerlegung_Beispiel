b65abb622e037a44c8a934eb99228e57c080a52c
Markus Zellner <markus@zellner-mail.de>
2020-11-16 14:50:30 +0100

    Upgrade jUnit Jupiter, rework as template

--- src/main/java/de/alamos/fe2/external/impl/Beispiel.java
+++ src/main/java/de/alamos/fe2/external/impl/Beispiel.java
@@ -9,7 +9,7 @@ import de.alamos.fe2.external.interfaces.IAlarmExtractor;
 /**
  * Zerlegung für Alarmfax der ILS Ansbach.
  */
-public class ILSAnsbach implements IAlarmExtractor {
+public class Beispiel implements IAlarmExtractor {
 
 	/**
 	 * Zerlegt den Textinput von der Texterkennung und extrahiert
@@ -29,40 +29,19 @@ public class ILSAnsbach implements IAlarmExtractor {
 		Map<String, String> data = new HashMap<>();
 		
 		/*
-		 * Beispielzerlegungen
+		 * Beispielzerlegung
 		 */
-		
-		//BEISPIEL 1
 
-		//Teile übergebenen String an Trennzeichen "&"
 		String[] parameters = input.split("&");
-		
-		//Stichwort steht an erster Stelle, daher [0]
+
 		String keyword = parameters[0];
-		data.put("keyword", keyword);
-		
-		//Straße steht an dritter Stelle, daher [2]
-		String street = parameters[2];
-		data.put("street", street);
+		data.put(EAlarmDataEntries.KEYWORD.getKey(), keyword);
 
-		
-		//BEISPIEL 2
-		/*
-		 * Hier werden die Parameter in der Map mit vordefinierten Enums aus dem Enum-Objekt "EAlarmDataEntries" gesetzt. 
-		 * Hier sind nicht alle Parameter hinterlegt. 
-		 * Sollte einer fehlen, verwenden Sie bitte den normalen Parametername, z. B. "mitteiler"
-		 */
-		
-		//Stichwort ist immer 6 Stellen lang und immer am Anfang der Nachricht
-		String stichwort = input.substring(0, 6);
-		data.put(EAlarmDataEntries.KEYWORD.getKey(), stichwort);
-		
-		//Straße wird extrahiert und anschließend werden alle Kommas
-		String strasse = input.substring(6, 30).replace(",", "");
-		data.put(EAlarmDataEntries.STREET.getKey(), strasse);
-		
+		String street = parameters[1];
+		data.put(EAlarmDataEntries.STREET.getKey(), street);
 
-		//Diese Zeile muss ganz am Ende stehen, nachdem die eigene Zerlegung durchgefßhrt wurde. Hier wird die Map an FE2 zurßck gegeben
+		data.put("someKey", "someValue");
+
 		return data;
 	}
 }
--- src/test/java/de/alamos/fe2/external/impl/test/BeispielTest.java
+++ src/test/java/de/alamos/fe2/external/impl/test/BeispielTest.java
@@ -1,39 +1,28 @@
 package de.alamos.fe2.external.impl.test;
 
-import static org.junit.Assert.assertEquals;
+import de.alamos.fe2.external.enums.EAlarmDataEntries;
+import de.alamos.fe2.external.impl.Beispiel;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 import java.util.Map;
 
-import de.alamos.fe2.external.impl.ILSAnsbach;
-import org.junit.Ignore;
-import org.junit.Test;
-
-import de.alamos.fe2.external.enums.EAlarmDataEntries;
-
-public class ILSAnsbachTest {
+public class BeispielTest {
 
 	@Test
-	@Ignore("Failing at the moment")
-	public void test() {
-		ILSAnsbach impl = new ILSAnsbach();
-
-		//BEISPIELDATEN
-		
-		String msg = "B 3 PERSON&Brand Zimmer&Teststrasse 12&Augsburg&EPN123&Anrufer meldet schwarzen Rauch aus Fenster";
+	public void extract_keywordIsExtracted() {
+		Beispiel impl = new Beispiel();
+		String msg = "B3&Teststraße";
 		Map<String, String> map = impl.extract(msg);
-		assertEquals("B 3 PERSON", map.get("keyword"));
-		assertEquals("Brand Zimmer", map.get("keyword_description"));
-		assertEquals("Teststraße 12", map.get(EAlarmDataEntries.STREET.getKey()));
-		assertEquals("Augsburg", map.get(EAlarmDataEntries.CITY.getKey()));
-		assertEquals("EPN123", map.get("epn"));
-		assertEquals("Anrufer meldet schwarzen Rauch aus Fenster", map.get("text"));
+		Assertions.assertEquals("B3", map.get(EAlarmDataEntries.KEYWORD.getKey()));
+	}
 
-		msg = "B BMA&Brandmeldeanlage (Feuermeldung)&Musterstrasse 12-14&Augsburg&EPN456";
-		map = impl.extract(msg);
-		assertEquals("B BMA", map.get("keyword"));
-		assertEquals("Brandmeldeanlage (Feuermeldung)", map.get(EAlarmDataEntries.KEYWORD_DESCRIPTION.getKey()));
-		assertEquals("Musterstrasse 12-14", map.get("street"));
-		assertEquals("Augsburg", map.get(EAlarmDataEntries.CITY.getKey()));
-		assertEquals("EPN456", map.get("epn"));
+	@Test
+	public void extract_streetIsExtracted() {
+		Beispiel impl = new Beispiel();
+
+		String msg = "B3&Teststraße";
+		Map<String, String> map = impl.extract(msg);
+		Assertions.assertEquals("Teststraße", map.get(EAlarmDataEntries.STREET.getKey()));
 	}
 }
